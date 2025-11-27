9432f57d03159c11433b3fad076a90bcbb7a0f24
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-11-27 15:22:06 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,11 +1,14 @@
 import sys
+
 if len(sys.argv) < 2:
     print("Usage: python even_odd_count.py <num1> <num2> <num3> ...")
     sys.exit(1)
 
 numbers = [int(n) for n in sys.argv[1:]]
+
 even_count = 0
 odd_count = 0
+
 for num in numbers:
     if num % 2 == 0:
         even_count += 1
@@ -13,4 +16,4 @@ for num in numbers:
         odd_count += 1
 
 print("Even Numbers Count:", even_count)
-print("Odd Numbers Count:", odd_count)
+print("Odd Numbers Count:", odd_count)
